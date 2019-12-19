b0fa20b810bab88817a6e300fe325e33386a7ae2
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-12-19 16:46:15 +0100

    Add more alert examples

--- lib/surface_demo_web/live/demo_live.ex
+++ lib/surface_demo_web/live/demo_live.ex
@@ -4,7 +4,14 @@ defmodule SurfaceDemoWeb.DemoLive do
   def render(assigns) do
     ~H"""
     <div>
-      <Alert>Hello World</Alert>
+      <Alert>Primary alert</Alert>
+      <Alert variant="secondary">Secondary alert</Alert>
+      <Alert variant="success">Success alert</Alert>
+      <Alert variant="danger">Danger alert</Alert>
+      <Alert variant="warning">Warning alert</Alert>
+      <Alert variant="info">Info alert</Alert>
+      <Alert variant="light">Light alert</Alert>
+      <Alert variant="dark">Dark alert</Alert>
     </div>
     """
   end
