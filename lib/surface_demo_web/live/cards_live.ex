f32736d700699841b179b3b82d1c68c6a8aa8840
Jørgen Orehøj Erichsen <joe@erichsen.net>
2020-01-01 13:36:49 +0100

    Added a sample code snippet and a link to the implementation

--- lib/surface_demo_web/live/cards_live.ex
+++ lib/surface_demo_web/live/cards_live.ex
@@ -6,9 +6,28 @@ defmodule SurfaceDemoWeb.CardsLive do
     <div class="row mb-4">
       <div class="col">
         <h2>Card component</h2>
+        <h4>Examples</h4>
         <Card title="Card title">
           Some quick example text to build on the card title and make up the bulk of the card's content.
         </Card>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h4>Code</h4>
+        <pre>
+          <code>
+            &lt;Card title=&quot;Card title&quot;&gt;
+              Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+            &lt;/Card&gt;
+          </code>
+        </pre>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col">
+        <h4>Implementation</h4>
+        <a href="https://github.com/joerichsen/surface_bootstrap_demo/blob/master/lib/surface_demo_web/components/card.ex">lib/surface_demo_web/components/card.ex</a>
       </div>
     </div>
     """
@@ -16,10 +35,5 @@ defmodule SurfaceDemoWeb.CardsLive do
 
   def mount(_session, socket) do
     {:ok, socket}
-  end
-
-  def handle_event("show_dialog", _, socket) do
-    Modal.show("modal")
-    {:noreply, socket}
   end
 end
