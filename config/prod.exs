7fd21b92ff9f64b9ea60c72224db7124851d5301
Jørgen Orehøj Erichsen <joe@erichsen.net>
2020-01-01 13:50:53 +0100

    Use the full name

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :surface_demo, SurfaceDemoWeb.Endpoint,
-  url: [scheme: "https", host: "surface-bootstrap-demo", port: 443],
+  url: [scheme: "https", host: "surface-bootstrap-demo.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
