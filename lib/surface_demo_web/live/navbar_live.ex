5ee899d0d744f04fc27da81c74eea90867a1eaa5
Jørgen Orehøj Erichsen <joe@erichsen.net>
2020-01-01 13:39:24 +0100

    Added a sample code snippet and a link to the implementation

--- lib/surface_demo_web/live/navbar_live.ex
+++ lib/surface_demo_web/live/navbar_live.ex
@@ -6,12 +6,34 @@ defmodule SurfaceDemoWeb.NavbarLive do
     <div class="row mb-4">
       <div class="col">
         <h2>Navbar component</h2>
+        <h4>Example</h4>
         <Navbar>
           <NavItem url="#" title="Home" active />
           <NavItem url="#" title="Features" />
           <NavItem url="#" title="Pricing" />
           <NavItem url="#" title="Disabled" disabled />
         </Navbar>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h4>Code</h4>
+        <pre>
+          <code>
+            &lt;Navbar&gt;
+              &lt;NavItem url=&quot;#&quot; title=&quot;Home&quot; active /&gt;
+              &lt;NavItem url=&quot;#&quot; title=&quot;Features&quot; /&gt;
+              &lt;NavItem url=&quot;#&quot; title=&quot;Pricing&quot; /&gt;
+              &lt;NavItem url=&quot;#&quot; title=&quot;Disabled&quot; disabled /&gt;
+            &lt;/Navbar&gt;
+          </code>
+        </pre>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col">
+        <h4>Implementation</h4>
+        <a href="https://github.com/joerichsen/surface_bootstrap_demo/blob/master/lib/surface_demo_web/components/navbar.ex">lib/surface_demo_web/components/navbar.ex</a>
       </div>
     </div>
     """
