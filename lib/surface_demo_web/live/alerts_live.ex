cf9dbf334627ab4efde6b2165f7833f3e55b36c8
Jørgen Orehøj Erichsen <joe@erichsen.net>
2020-01-01 13:33:54 +0100

    Added a sample code snippet and a link to the implementation

--- lib/surface_demo_web/live/alerts_live.ex
+++ lib/surface_demo_web/live/alerts_live.ex
@@ -3,9 +3,10 @@ defmodule SurfaceDemoWeb.AlertsLive do
 
   def render(assigns) do
     ~H"""
-    <div class="row mb-4">
+    <div class="row">
       <div class="col">
         <h2>Alert component</h2>
+        <h4>Examples</h4>
         <Alert>Primary alert</Alert>
         <Alert variant="secondary">Secondary alert</Alert>
         <Alert variant="success">Success alert</Alert>
@@ -14,6 +15,29 @@ defmodule SurfaceDemoWeb.AlertsLive do
         <Alert variant="info">Info alert</Alert>
         <Alert variant="light">Light alert</Alert>
         <Alert variant="dark">Dark alert</Alert>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h4>Code</h4>
+        <pre>
+          <code>
+            &lt;Alert&gt;Primary alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;secondary&quot;&gt;Secondary alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;success&quot;&gt;Success alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;danger&quot;&gt;Danger alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;warning&quot;&gt;Warning alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;info&quot;&gt;Info alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;light&quot;&gt;Light alert&lt;/Alert&gt;
+            &lt;Alert variant=&quot;dark&quot;&gt;Dark alert&lt;/Alert&gt;
+          </code>
+        </pre>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col">
+        <h4>Implementation</h4>
+        <a href="https://github.com/joerichsen/surface_bootstrap_demo/blob/master/lib/surface_demo_web/components/alert.ex">lib/surface_demo_web/components/alert.ex</a>
       </div>
     </div>
     """
