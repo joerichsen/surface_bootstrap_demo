3d9e9e886eb85d7f59d964d683fc8a96d0f12646
Jørgen Orehøj Erichsen <joe@erichsen.net>
2020-01-01 13:45:12 +0100

    Fix SSL stuff

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :surface_demo, SurfaceDemoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "surface-bootstrap-demo", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
